91f3aebf60406e8aff5bb6da6be59658ceeb1fa8
Tom <tom@aeragen.com>
2021-12-07 15:24:19 -0500

    Add project files.

---
+++ RPiTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RPiTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
